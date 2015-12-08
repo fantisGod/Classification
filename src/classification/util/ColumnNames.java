0f30caa06a2e8c0e2b2b5c322fe2d8a5a89fd371
fantisGod <faithlong@163.com>
2015-12-08 17:36:34 +0800

    fantisGod

--- src/classification/util/ColumnNames.java
+++ src/classification/util/ColumnNames.java
@@ -9,6 +9,7 @@ private static final long serialVersionUID = 6509035934111193396L;
 	public ColumnNames(String line){
 		String[] columnNames = line.split(";");
 		for(String column: columnNames){
+			column=column.replaceAll("\"","");
 			this.add(column);
 		}
 	}
